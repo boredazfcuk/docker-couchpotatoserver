fcc95066ddbbbe5934099d401cefd81de923cb6d
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-04-18 23:16:01 +0100

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-if [ "$(nc -z "$(hostname -i)" 5050; echo $?)" -ne 0 ]; then
+if [ "$(netstat -plnt | grep -c 5050)" -ne 1 ]; then
    echo "CouchPotato WebUI not responding on port 5050"
    exit 1
 fi

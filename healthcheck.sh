de9ebb312f212838ebb580f7b5d39755f55b7fb5
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-08-07 11:33:03 +0100

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,5 +5,10 @@ if [ "$(netstat -plnt | grep -c 5050)" -ne 1 ]; then
    exit 1
 fi
 
+if [ "$(ip -o addr | grep "$(hostname -i)" | wc -l)" -eq 0 ]; then
+   echo "NIC missing"
+   exit 1
+fi
+
 echo "CouchPotato WebUI responding on port 5050"
 exit 0
